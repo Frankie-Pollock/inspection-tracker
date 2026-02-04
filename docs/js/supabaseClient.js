0dc041169db0b7d67785f3af8652486d2c554f36
Frankie-Pollock <126087959+Frankie-Pollock@users.noreply.github.com>
2026-02-04 11:15:26 +0000

    Update supabaseClient.js

--- docs/js/supabaseClient.js
+++ docs/js/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  "https://YOUR_PROJECT_ID.supabase.co",
-  "YOUR_ANON_PUBLIC_KEY"
+  "https://https://yrykxgavwulolfngiltc.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyeWt4Z2F2d3Vsb2xmbmdpbHRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxOTczMTcsImV4cCI6MjA4NTc3MzMxN30.hIwP2wCS2xNgnVIZ65QQHhRGbrbzjwPRIE1GIiR4lu4"
 );
