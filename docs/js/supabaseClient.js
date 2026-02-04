807a1c953135886e79f53de76a14a70d4157aa8e
Frankie-Pollock <126087959+Frankie-Pollock@users.noreply.github.com>
2026-02-04 11:28:19 +0000

    Update supabaseClient.js

--- docs/js/supabaseClient.js
+++ docs/js/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  "https://https://yrykxgavwulolfngiltc.supabase.co",
+  "sb_publishable_l2MmiKmenn-auprAwbg6NQ__m1D6wWY",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyeWt4Z2F2d3Vsb2xmbmdpbHRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxOTczMTcsImV4cCI6MjA4NTc3MzMxN30.hIwP2wCS2xNgnVIZ65QQHhRGbrbzjwPRIE1GIiR4lu4"
 );
